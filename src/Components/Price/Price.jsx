de090fc2112206ab1c9e35821be8f7fec5acb3dd
NataliiaDanchenko <danchnataly@gmail.com>
2023-05-19 02:33:13 +0300

    commit new-11

---
+++ src/Components/Price/Price.jsx
@@ -0,0 +1,32 @@
+import "./Price.css";
+import PriceCard from "./PriceCard/PriceCard";
+
+const Price = (props) => {
+    const priceCard = props.pricePrice.map(pc => <PriceCard pc={pc}/>);
+  return (
+      <div className="container">
+          <div className="title">СТОИМОСТЬ УСЛУГ</div>
+          <div className="price_flex">
+              <div className="price_input">
+                  <div className="price_title">{props.price.title1}</div>
+                  <div className="price_subtitle">{props.price.subTitle}</div>
+                  <div>
+                      <form className="form">
+                          <div><input type="text" placeholder="Name"/></div>
+                          <div><input type="text" placeholder="Phone"/></div>
+                          <div><input type="text" placeholder="Email"/></div>
+                          <div className="price_button">{props.price.button1}</div>
+                    </form>
+                  </div>
+              </div>
+
+              <div className="price_card_flex">
+                  {priceCard}
+              </div>
+          </div>
+      </div>
+
+  )
+}
+
+export default Price;
---
+++ src/Components/Price/PriceCard/PriceCard.jsx
@@ -0,0 +1,14 @@
+import "./PriceCard.css";
+
+const PriceCard = (props) => {
+  return (
+      <div className="price_card">
+        <div className="price_title">{props.pc.title}</div>
+        <div className="price_text">{props.pc.text}</div>
+        <div className="price_price">{props.pc.price}</div>
+          <div className="price_button">{props.pc.button}</div>
+      </div>
+  )
+}
+
+export default PriceCard;
